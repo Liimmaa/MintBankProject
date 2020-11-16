718fbac1df7cc2e3536c497463158f75b742f329
liimmaa <halimammegwa@gmail.com>
2020-11-16 19:38:31 +0100

    Added a view above the table view

--- MintProject/MintProject/ListCommitsViewController.swift
+++ MintProject/MintProject/ListCommitsViewController.swift
@@ -11,17 +11,48 @@ import SnapKit
 class ListCommitsViewController: UIViewController, UITableViewDelegate,  UITableViewDataSource, GithubCommitProtocol {
 
     var tableView = UITableView()
+    var titleView = UIView()
+    var imageView = UIImageView()
     var commitData: [WelcomeElement]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        setupTitleview()
+        setupImageView()
         setupTableview()
         
         let githubCommitLoader = DataLoader()
         githubCommitLoader.loadDataDelegate = self
         githubCommitLoader.getData()
+    }
+    
+    func setupTitleview() {
+        view.addSubview(titleView)
+        titleView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        titleView.snp.makeConstraints { (make) in
+            make.top.equalTo(view.snp.top).offset(50)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+            make.bottom.equalTo(view.snp.bottom)
+            make.height.equalTo(view.snp.height).multipliedBy(0.15)
+        }
+    }
+    
+    func setupImageView(){
+        titleView.addSubview(imageView)
+        imageView.backgroundColor = #colorLiteral(red: 0.9505546704, green: 0.9505546704, blue: 0.9505546704, alpha: 1)
+        imageView.layer.cornerRadius = 10
+        imageView.image = UIImage(named: "ruby2")
+        imageView.contentMode = .scaleAspectFill
+        imageView.snp.makeConstraints { (make) in
+            make.top.equalTo(titleView)
+            make.left.equalTo(titleView)
+            make.right.equalTo(titleView)
+            make.bottom.equalTo(titleView)
+        }
     }
 
     func setupTableview() {
@@ -32,7 +63,7 @@ class ListCommitsViewController: UIViewController, UITableViewDelegate,  UITable
         tableView.dataSource = self
         tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
         tableView.snp.makeConstraints { (make) in
-            make.top.equalTo(view.snp.top)
+            make.top.equalTo(titleView.snp.bottom).offset(0)
             make.left.equalTo(view.snp.left)
             make.right.equalTo(view.snp.right)
             make.bottom.equalTo(view.snp.bottom)
