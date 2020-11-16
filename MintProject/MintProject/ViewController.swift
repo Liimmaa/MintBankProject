41620e06f85cd864e887bf827119cb0a6a7ef97c
liimmaa <halimammegwa@gmail.com>
2020-11-16 18:04:12 +0100

    Added Xcode Project

---
+++ MintProject/MintProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MintProject
+//
+//  Created by USER on 16/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
