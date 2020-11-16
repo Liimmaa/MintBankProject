6b5329f0d14c93d9ea04c16a53659f7b6883f00a
liimmaa <halimammegwa@gmail.com>
2020-11-16 18:40:31 +0100

    Setup tableView cell

--- MintProject/MintProject/ListCommitsViewController.swift
+++ MintProject/MintProject/ListCommitsViewController.swift
@@ -6,15 +6,29 @@
 //
 
 import UIKit
+import SnapKit
 
 class ListCommitsViewController: UIViewController {
 
+    var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
         view.backgroundColor = .blue
+        setupTableview()
     }
 
-
+    func setupTableview() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        view.addSubview(tableView)
+        tableView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        tableView.snp.makeConstraints { (make) in
+            make.top.equalTo(view.snp.top)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+            make.bottom.equalTo(view.snp.bottom)
+        }
+    }
 }
 
