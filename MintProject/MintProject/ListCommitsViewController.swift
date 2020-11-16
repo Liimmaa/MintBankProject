f0e57ad15f2227940a3e544a716c07ef7c256d0f
liimmaa <halimammegwa@gmail.com>
2020-11-16 18:58:52 +0100

    Setup custom cell

--- MintProject/MintProject/ListCommitsViewController.swift
+++ MintProject/MintProject/ListCommitsViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class ListCommitsViewController: UIViewController {
+class ListCommitsViewController: UIViewController, UITableViewDelegate,  UITableViewDataSource {
 
     var tableView = UITableView()
     
@@ -20,15 +20,35 @@ class ListCommitsViewController: UIViewController {
     }
 
     func setupTableview() {
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableView.register(CommitsCell.self, forCellReuseIdentifier: "cellId")
         view.addSubview(tableView)
         tableView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
         tableView.snp.makeConstraints { (make) in
             make.top.equalTo(view.snp.top)
             make.left.equalTo(view.snp.left)
             make.right.equalTo(view.snp.right)
             make.bottom.equalTo(view.snp.bottom)
         }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! CommitsCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
 
