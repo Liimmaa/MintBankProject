602db8332a312908f2c258bc234c1ba9f9b3a849
liimmaa <halimammegwa@gmail.com>
2020-11-16 18:28:12 +0100

    Hijacked Storyboard

---
+++ MintProject/MintProject/ListCommitsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MintProject
+//
+//  Created by Lima on 16/11/2020.
+//
+
+import UIKit
+
+class ListCommitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .blue
+    }
+
+
+}
+
